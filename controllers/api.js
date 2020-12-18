bdf2341b4b6fb5fbdd5337aa197f8026bca4a6f5
bravis85 <73161499+bravis85@users.noreply.github.com>
2020-12-18 16:45:00 +0000

    Everything repaired - next define all db models

--- controllers/api.js
+++ controllers/api.js
@@ -1,13 +1,12 @@
 const models = require("../models");
 
 exports.get_home = function (req, res) {
-    res.render('index', { title: 'Express' });
-}
+  res.render("index", { title: "Express" });
+};
 
-exports.get_peeps = function (req, res) {
-    return models.family_member.findAll()
-        .then((familymembers) => {
-            console.log(familymembers);
-            res.json(familymembers);
-        })
-}
+exports.get_fam = function (req, res) {
+  return models.family_member.findAll().then((familymembers) => {
+    console.log(familymembers);
+    res.json(familymembers);
+  });
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+let api = require("../controllers/api");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get("/", api.get_home);
+router.get("/get_fam", api.get_fam);
 
 module.exports = router;
