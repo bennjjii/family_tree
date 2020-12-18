36d84df12d17845a45ba9bfda5d83cdc5df3441b
bravis85 <73161499+bravis85@users.noreply.github.com>
2020-12-18 16:56:17 +0000

    deaths model added

---
+++ models/deaths.js
@@ -0,0 +1,33 @@
+"use strict";
+
+module.exports = (sequelize, DataTypes) => {
+  var death = sequelize.define("death", {
+    uuid_death: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    d_o_d: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+  });
+
+  death.associate = (models) => {
+    death.belongsTo(models.family_member, {
+      foreignKey: {
+        name: "died",
+        allowNull: false,
+      },
+    });
+    death.belongsTo(models.family_account, {
+      foreignKey: {
+        name: "uuid_family_account",
+        allowNull: false,
+      },
+    });
+  };
+
+  return death;
+};
