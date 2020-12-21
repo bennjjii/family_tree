ff2c8cfed054bb0fa72187b28cf4aaf3496d2459
bravis85 <73161499+bravis85@users.noreply.github.com>
2020-12-21 01:57:11 +0000

    Login finished, idcard actually added

---
+++ client/src/components/IdCard.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+export class IdCard extends Component {
+  render() {
+    return (
+      <div className="IdCard">
+        <img src="" />
+        <h1>Name</h1>
+      </div>
+    );
+  }
+}
+
+export default IdCard;
