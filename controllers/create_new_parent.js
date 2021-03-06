3192027d5411eaa7896cf94953e97171a9000be1
bravis85 <73161499+bravis85@users.noreply.github.com>
2021-03-06 19:03:43 +0000

    create_new_parent moved to separate file

---
+++ controllers/create_new_parent.js
@@ -0,0 +1,62 @@
+const models = require("../models");
+require("dotenv").config();
+const uuid_family_tree = process.env.TEMP_UUID_FAMILY_TREE;
+
+exports.create_new_parent = (req, res) => {
+  let genderSelecto = null;
+  let genderSelector = null;
+  let responses = [];
+
+  if (req.body.np_gender === "Male") {
+    genderSelecto = "fathe";
+    genderSelector = "father";
+  } else {
+    genderSelecto = "mothe";
+    genderSelector = "mother";
+  }
+
+  return models.family_member
+    .create({
+      first_name: req.body.np_first_name,
+      middle_name: req.body.np_middle_name,
+      last_name: req.body.np_last_name,
+      gender: req.body.np_gender,
+      uuid_family_tree: uuid_family_tree,
+    })
+    .then((parent) => {
+      responses.push(JSON.parse(JSON.stringify(parent)));
+      return models.birth.update(
+        {
+          [genderSelector]: responses[0].uuid_family_member,
+        },
+        {
+          where: {
+            uuid_birth: req.body.uuid_birth,
+          },
+        }
+      );
+    })
+    .then((resp) => {
+      responses.push(JSON.parse(JSON.stringify(resp)));
+
+      return models.birth.create({
+        child: responses[0].uuid_family_member,
+        d_o_b: req.body.d_o_b ? req.body.d_o_b.split("T")[0] : null,
+        uuid_family_tree: uuid_family_tree,
+      });
+    })
+    .then((resp) => {
+      responses.push(JSON.parse(JSON.stringify(resp)));
+      let response = {
+        [genderSelector]: {
+          name: [
+            responses[0].first_name,
+            responses[0].middle_name,
+            responses[0].last_name,
+          ],
+          uuid: responses[0].uuid_family_member,
+        },
+      };
+      res.json(response);
+    });
+};
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,12 @@ var router = express.Router();
 let api = require("../controllers/api");
 let { get_target_data } = require("../controllers/get_target_data");
 let { create_new_child } = require("../controllers/create_new_child");
+let { create_new_parent } = require("../controllers/create_new_parent");
 
 router.get("/get_target_data/:id", get_target_data);
 router.post("/add_family_member", api.create_family_member);
 router.post("/create_new_child", create_new_child);
-router.post("/create_new_parent", api.create_new_parent);
+router.post("/create_new_parent", create_new_parent);
 router.post("/create_new_spouse", api.create_new_spouse);
 router.post("/register", api.register);
 router.post("/login", api.login);
