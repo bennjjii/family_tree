33f840cc3b9a3e36f49a681189867aeb1d538dd5
bravis85 <73161499+bravis85@users.noreply.github.com>
2021-03-07 19:05:41 +0000

    ProtectedRoute added

---
+++ client/src/components/ProtectedRoute.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { Route, Redirect } from "react-router-dom";
+
+const ProtectedRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={(props) => {
+        if (false) {
+          return <Component {...props} />;
+        } else {
+          return (
+            <Redirect
+              to={{
+                pathname: "/",
+                state: {
+                  from: props.location,
+                },
+              }}
+            />
+          );
+        }
+      }}
+    />
+  );
+};
+
+export default ProtectedRoute;
