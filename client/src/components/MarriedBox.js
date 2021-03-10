b13d19b1d01328c0b3211f341f9b8731594cabff
bravis85 <73161499+bravis85@users.noreply.github.com>
2021-03-10 19:24:14 +0000

    MarriedBox converted to functional component

--- client/src/components/MarriedBox.js
+++ client/src/components/MarriedBox.js
@@ -1,37 +1,31 @@
 import React, { Component } from "react";
 
-export class MarriedBox extends Component {
-  constructor(props) {
-    super();
-  }
-
-  render() {
-    return (
-      <div className="married_box">
-        <button className="plus_button" onClick={this.props.showNewSpouse}>
-          +
-        </button>
-        <h5>Married:</h5>
-        {this.props.spouses &&
-          this.props.spouses.map((spouse) => {
-            return (
-              <>
-                <button
-                  className="parents-btn"
-                  uuid={spouse.uuid}
-                  onClick={this.props.handleUpd}
-                >
-                  {spouse.name.join(" ")}
-                  <br />
-                  {spouse.d_o_mar}
-                </button>
+function MarriedBox(props) {
+  return (
+    <div className="married_box">
+      <button className="plus_button" onClick={props.showNewSpouse}>
+        +
+      </button>
+      <h5>Married:</h5>
+      {props.spouses &&
+        props.spouses.map((spouse) => {
+          return (
+            <>
+              <button
+                className="parents-btn"
+                uuid={spouse.uuid}
+                onClick={props.handleUpd}
+              >
+                {spouse.name.join(" ")}
                 <br />
-              </>
-            );
-          })}
-      </div>
-    );
-  }
+                {spouse.d_o_mar}
+              </button>
+              <br />
+            </>
+          );
+        })}
+    </div>
+  );
 }
 
 export default MarriedBox;
