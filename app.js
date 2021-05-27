b7058a724f8eb1b088d3157fff2b052741ca5ea1
bravis85 <73161499+bravis85@users.noreply.github.com>
2021-05-27 12:19:03 +0100

    Added to static.json

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(
 app.use(express.static(path.resolve(__dirname, "./client/build")));
 
 //should match all routes here?
-app.use("/*", indexRouter);
+app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
