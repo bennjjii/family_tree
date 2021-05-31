40552cc07df496e8b5a08670e2db4b026eb8dd5f
bravis85 <73161499+bravis85@users.noreply.github.com>
2021-05-31 14:04:20 +0100

    Moved all the css into index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./normalize.css";
-import "./index_new.css";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-datepicker/dist/react-datepicker.css";
 import App from "./App";
+import "./index_new.css";
 
 require("dotenv").config();
 
