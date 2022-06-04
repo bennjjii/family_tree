9c44e545af05698c38180914f589015cffa00c43
Ben <73161499+bennjjii@users.noreply.github.com>
2022-06-04 11:46:52 +0100

    Bugfix: something to do with out of date package lock

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = (props) => {
         className="navbar-brand"
         onClick={() => auth.clearShowPublic(history)}
       >
-        HouseOfMe
+        HouseofMe
       </Link>
       <div className="navbar-collapse">
         <ul className="navbar-nav ml-auto">
