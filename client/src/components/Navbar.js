f6ed381e95b6a975e1c8e721df2383ae11a21764
Benji <73161499+bennjjii@users.noreply.github.com>
2023-07-27 19:17:05 +0100

    Change branding

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = (props) => {
         className="navbar-brand"
         onClick={() => auth.clearShowPublic(history)}
       >
-        HouseofMe
+        familytrees.io
       </Link>
       <div className="navbar-collapse">
         <ul className="navbar-nav ml-auto">
